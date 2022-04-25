ce32c6485f707d61c830db3ef5d43d93df53e476
billadriang <billadriang@gmail.com>
2022-04-24 23:53:30 -0400

    Objetos y arrays agregados

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 // alert()
 
-// VR Glicemia
-let B_gli = (80);
-let T_gli = (110);
+// // VR Glicemia
+// let glicemia.valorInferior = (80);
+// let glicemia.valorSuperior = (110);
 
-// VR Colesterol total
-const B_colTotal = (125);
-const T_colTotal = (200);
+// // VR Colesterol total
+// const B_colTotal = (125);
+// const T_colTotal = (200);
 
-// VR Hemoglobina
-const B_hbHombre = (13.8);
-const T_hbHombre = (17.2);
+// // VR Hemoglobina
+// const B_hbHombre = (13.8);
+// const T_hbHombre = (17.2);
 
-const B_hbMujer = (12.1);
-const T_hbMujer = (15.1);
+// const B_hbMujer = (12.1);
+// const T_hbMujer = (15.1);
 
+
+class ValorReferencia {
+    constructor(valorInferior,valorSuperior,bajo,normal,alto) {
+        this.valorInferior = valorInferior;
+        this.valorSuperior = valorSuperior;
+        this.bajo = bajo;
+        this.normal = normal;
+        this.alto = alto;
+    }
+}
+
+
+// let VALOR A AGREGAR = new ValorReferencia(RANGO INFERIOR, RANGO SUPERIOR, MENSAJE VALOR BAJO, MENSAJE VALOR NORMAL, MENSAJE VALOR ALTO)
+
+let glicemia = new ValorReferencia(80,110,"Estas en hipoglicemia, el Valor de Referencia de la glicemia va de  80 a 110 mg/dL","Estas dentro de los valores de Referencia para Glucosa en Sangre, los cuales son de 80 a 110mg/dL","Estas en Hiperglicemia, tus valores estan por encima de los de referencia para glucosa en sangre, los cuales son de 80 a 110mg/dL")
+
+let colesterol = new ValorReferencia(125,200,"Estas en hipocolesterolemia, el Valor de Referencia del colesterol serico va de 125 a 200mg/dL","Estas dentro de los valores de Referencia para el colesterol total serico, los cuales son de 125 a 200mg/dL","Estas en hipercolesterolemia, tus valores estan por encima de los de referencia para Colesterol en sangre, los cuales son de 125 a 200mg/dL")
+
+let hemoglobinaHombre = new ValorReferencia(13.8,17.2,"Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en hombres va de 13.8 a 17.2mg/dL","Estas dentro de los valores de referencia de Hemoglobina para hombre los cuales son 13.8 a 17.2mg/dL","Tu valor de hemoglobina esta por encima de los de referencia para hombres, los cuales van de 13.8 a 17.2mg/dL")
+
+let hemoglobinaMujer = new ValorReferencia(12.1,15.1,"Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en mujeres va de 12.1 a 15.1mg/dL","Estas dentro de los valores de referencia de Hemoglobina para mujeres los cuales son 12.1 a 15.1mg/dL","Tu valor de hemoglobina esta por encima de los de referencia para mujeres, los cuales van de 12.1 a 15.1mg/dL")
+
+
+
+//ARRAY DE VALORES PROXIMOS
 let pruebas = [`<h2>Por favor escribe alguna prueba, tenemos estas:<br><br></h2>Glicemia`, "Colesterol", "Hemoglobina", "<br><br>Proximamente:", "Trigliceridos", "Colesterol HDL", "Colesterol LDL", "VLDL", "Hematocrito", "Contaje Globulos Rojos", "Contaje Leucocitos", "Valores de Hemograma", "Y muchos mas."]
 
 
@@ -27,14 +52,14 @@ let calcularValor = () => {
     switch (tst) {
         case "glicemia":
             let valor = prompt("Cual es tu valor de glicemia?").trim();
-            if (valor <= B_gli) {
-                lab.innerHTML = "Estas en hipoglicemia, el Valor de Referencia de la glicemia va de " + B_gli + " a " + T_gli + "mg/dL <br> y la tuya esta en " + valor + "mg/dL";
+            if (valor <= glicemia.valorInferior) {
+                lab.innerHTML = `${glicemia.bajo}<br> y la tuya esta en ${valor}mg/dL`;
                 break;
-            } else if (valor > B_gli && valor < T_gli) {
-                lab.innerHTML = "Estas dentro de los valores de Referencia para Glucosa en Sangre, los cuales son " + B_gli + " a " + T_gli + "mg/dL <br> y la tuya esta en " + valor + "mg/dL";
+            } else if (valor > glicemia.valorInferior && valor < glicemia.valorSuperior) {
+                lab.innerHTML = `${glicemia.normal}<br> y la tuya esta en ${valor}mg/dL`;
                 break;
-            } else if (valor >= T_gli) {
-                lab.innerHTML = "Estas en Hiperglicemia, tus valores estan por encima de los de referencia para glucosa en sangre, los cuales son " + B_gli + " a " + T_gli + "mg/dL <br> y la tuya esta en " + valor + "mg/dL<br>Atent@ a una posible diabetes, si estos valores son cronicos deberias ir a un Endocrinologo";
+            } else if (valor >= glicemia.valorSuperior) {
+                lab.innerHTML = `${glicemia.alto}<br> y la tuya esta en ${valor}mg/dL<br>Atent@ a una posible diabetes, si estos valores son cronicos deberias ir a un Endocrinologo`;
                 break;
             } else {
                 lab.innerHTML = "Por favor ingesa un numero valido";
@@ -42,14 +67,14 @@ let calcularValor = () => {
             }
             case "colesterol":
                 let valorc = prompt("Cual es tu valor de Colesterol Total?").trim();
-                if (valorc <= B_colTotal) {
-                    lab.innerHTML = "Estas en hipocolesterolemia, el Valor de Referencia del colesterol serico va de " + B_colTotal + " a " + T_colTotal + "mg/dL<br> y el tuyo esta en " + valorc + "mg/dL";
+                if (valorc <= colesterol.valorInferior) {
+                    lab.innerHTML = `${colesterol.bajo}<br> y el tuyo esta en ${valorc}mg/dL`;
                     break;
-                } else if (valorc > B_colTotal && valorc < T_colTotal) {
-                    lab.innerHTML = "Estas dentro de los valores de Referencia para el colesterol total serico, los cuales son " + B_colTotal + " a " + T_colTotal + "mg/dL<br> y el tuyo esta en " + valorc + "mg/dL";
+                } else if (valorc > colesterol.valorInferior && valorc < colesterol.valorSuperior) {
+                    lab.innerHTML = `${colesterol.normal}<br> y el tuyo esta en ${valorc}mg/dL`;
                     break;
-                } else if (valorc >= T_colTotal) {
-                    lab.innerHTML = "Estas en hipercolesterolemia, tus valores estan por encima de los de referencia para Colesterol en sangre, los cuales son " + B_colTotal + " a " + T_colTotal + "mg/dL<br> y el tuyo esta en " + valorc + "mg/dL<br>Atent@, esto junto a obesidad abdominal aumentan los riesgos de un accidente cardiovascular";
+                } else if (valorc >= colesterol.valorSuperior) {
+                    lab.innerHTML = `${colesterol.alto}<br> y el tuyo esta en ${valorc}mg/dL<br>Atent@, esto junto a obesidad abdominal aumentan los riesgos de un accidente cardiovascular`;
                     break;
                 } else {
                     lab.innerHTML = "Por favor ingesa un numero valido";
@@ -60,14 +85,14 @@ let calcularValor = () => {
 
                     if (sex == "hombre") {
                         let valorh = prompt("Cual es tu valor de hemoglobina?");
-                        if (valorh <= B_hbHombre) {
-                            lab.innerHTML = "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en hombres va de " + B_hbHombre + " a " + T_hbHombre + "mg/dL<br> y la tuya esta en " + valorh + "mg/dL, esta esta muy relacionada a anemias";
+                        if (valorh <= hemoglobinaHombre.valorInferior) {
+                            lab.innerHTML = `${hemoglobinaHombre.bajo}<br> y la tuya esta en ${valorh}mg/dL, estos valores estan muy relacionados a anemias`;
                             break;
-                        } else if (valorh > B_hbHombre && valorh < T_hbHombre) {
-                            lab.innerHTML = "Estas dentro de los valores de Referencia Hemoglobina en hombres, los cuales son " + B_hbHombre + " a " + T_hbHombre + "mg/dL<br> la tuya esta en " + valorh + "mg/dL";
+                        } else if (valorh > hemoglobinaHombre.valorInferior && valorh < hemoglobinaHombre.valorSuperior) {
+                            lab.innerHTML = `${hemoglobinaHombre.normal}<br> la tuya esta en ${valorh}mg/dL`;
                             break;
-                        } else if (valorh >= T_hbHombre) {
-                            lab.innerHTML = "Tu valor de hemoglobina esta por encima de los de referencia para hombres, los cuales son " + B_hbHombre + " a " + T_hbHombre + "mg/dL<br> y la tuya esta en " + valorh + "mg/dL<br>Atento, este es un clasico sintoma de Policitemia vera, deberias llevarle tus resultados a un Hematologo";
+                        } else if (valorh >= hemoglobinaHombre.valorSuperior) {
+                            lab.innerHTML = `${hemoglobinaHombre.alto}<br> y la tuya esta en ${valorh}mg/dL<br>Atento, este es un clasico sintoma de Policitemia vera, deberias llevarle tus resultados a un Hematologo`;
                             break;
                         } else {
                             lab.innerHTML = "Por favor ingesa un numero valido";
@@ -75,14 +100,14 @@ let calcularValor = () => {
                         }
                     } else if (sex == "mujer") {
                         let valorh = prompt("Cual es tu valor de hemoglobina?").trim();
-                        if (valorh <= B_hbMujer) {
-                            lab.innerHTML = "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en mujeres va de " + B_hbMujer + " a " + T_hbMujer + "mg/dL<br> y la tuya mi amor, esta en " + valorh + "mg/dL, esta esta muy relacionada a anemias";
+                        if (valorh <= hemoglobinaMujer.valorInferior) {
+                            lab.innerHTML = `${hemoglobinaMujer.bajo}<br> y la tuya mi amor, esta en ${valorh}mg/dL, esta esta muy relacionada a anemias`;
                             break;
-                        } else if (valorh > B_hbMujer && valorh < T_hbMujer) {
-                            lab.innerHTML = "Estas dentro de los valores de Referencia de Hemoglobina en mujeres, los cuales son " + B_hbMujer + " a " + T_hbMujer + "mg/dL<br> y la tuya esta en " + valorh + "mg/dL";
+                        } else if (valorh > hemoglobinaMujer.valorInferior && valorh < hemoglobinaMujer.valorSuperior) {
+                            lab.innerHTML = `${hemoglobinaMujer.normal}<br> y la tuya esta en ${valorh}mg/dL`;
                             break;
-                        } else if (valorh >= T_hbMujer) {
-                            lab.innerHTML = "Tu valor de hemoglobina esta por encima de los de referencia para mujeres, los cuales son " + B_hbMujer + " a " + T_hbMujer + "mg/dL<br> y la tuya esta en " + valorh + "mg/dL<br>Atenta, este es un clasico sintoma de Policitemia vera, deberias llevarle tus resultados a un Hematologo";
+                        } else if (valorh >= hemoglobinaMujer.valorSuperior) {
+                            lab.innerHTML = `${hemoglobinaMujer.alto}<br> y la tuya esta en ${valorh}mg/dL<br>Atenta, este es un clasico sintoma de Policitemia vera, deberias llevarle tus resultados a un Hematologo`;
                             break
                         } else {
                             lab.innerHTML = "Por favor ingesa un numero valido";
@@ -101,7 +126,7 @@ let calcularValor = () => {
                         }, 5000);
                         //For loop para mostrar pruebas disponibles 
                         for (i = 0; i < pruebas.length; i = i + 1)
-                            document.write("<br>" + pruebas[i] + "<br>")
+                            lab.innerHTML += "<br>" + pruebas[i] + "<br>"
                         break;
 
 
