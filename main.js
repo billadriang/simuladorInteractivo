9b12b96ca4d98a09e62385e3e20369648907a903
billadriang <billadriang@gmail.com>
2022-05-16 23:53:30 -0400

    Agregadas librerias, tostify y sweetalert2

--- main.js
+++ main.js
@@ -1,75 +1,24 @@
-
-// PLANTILLA DE VALORES DE REFERENCIA 
-class ValorReferencia {
-    constructor(name, valorInferior, valorSuperior, bajo, normal, alto) {
-        this.name = name;
-        this.valorInferior = valorInferior;
-        this.valorSuperior = valorSuperior;
-        this.bajo = bajo;
-        this.normal = normal;
-        this.alto = alto;
-    }
-}
-// let VALOR A AGREGAR = new ValorReferencia(RANGO INFERIOR, RANGO SUPERIOR, MENSAJE VALOR BAJO, MENSAJE VALOR NORMAL, MENSAJE VALOR ALTO
-let glicemia = new ValorReferencia(
-    "GLICEMIA O GLUCEMIA",
-    80,
-    110,
-    "Estas en hipoglicemia, el Valor de Referencia de la glicemia va de  80 a 110 mg/dL",
-    "Estas dentro de los valores de Referencia para Glucosa en Sangre, los cuales son de 80 a 110mg/dL",
-    "Estas en Hiperglicemia, tus valores estan por encima de los de referencia para glucosa en sangre, los cuales son de 80 a 110mg/dL"
-);
-
-let colesterol = new ValorReferencia(
-    "COLESTEROL TOTAL",
-    125,
-    200,
-    "Estas en hipocolesterolemia, el Valor de Referencia del colesterol serico va de 125 a 200mg/dL",
-    "Estas dentro de los valores de Referencia para el colesterol total serico, los cuales son de 125 a 200mg/dL",
-    "Estas en hipercolesterolemia, tus valores estan por encima de los de referencia para Colesterol en sangre, los cuales son de 125 a 200mg/dL"
-);
-
-let hemoglobinaHombre = new ValorReferencia(
-    "HEMOGLOBINA EN HOMBRES",
-    13.8,
-    17.2,
-    "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en hombres va de 13.8 a 17.2mg/dL",
-    "Estas dentro de los valores de referencia de Hemoglobina para hombre los cuales son 13.8 a 17.2mg/dL",
-    "Tu valor de hemoglobina esta por encima de los de referencia para hombres, los cuales van de 13.8 a 17.2mg/dL"
-);
-
-let hemoglobinaMujer = new ValorReferencia(
-    "HEMOGLOBINA EN MUJERES",
-    12.1,
-    15.1,
-    "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en mujeres va de 12.1 a 15.1mg/dL",
-    "Estas dentro de los valores de referencia de Hemoglobina para mujeres los cuales son 12.1 a 15.1mg/dL",
-    "Tu valor de hemoglobina esta por encima de los de referencia para mujeres, los cuales van de 12.1 a 15.1mg/dL"
-);
-
-let neutrofilos = new ValorReferencia(
-    "SEGMENTADOS NEUTROFILOS",
-    2000,
-    7500,
-    "Estas cursando con Neutropenia, Segmentados Neutrofilos bajos, el Valor de referencia de neutrofilos va de 2000 a 7500 celulas por mL de sangre",
-    "Tus valores de Segmentados neutrofilos se encuentran dentro de los valores normales  de 2000 a 7500 celulas por mL de sangre",
-    "Cursas con una Neutrofilia, considerable aumento de los Segmentados Neutrofilos en sangre, es normalmente causada por infecciones recientes, o inclusive enfermedades inflamatorias "
-);
+import {glicemia, colesterol, hemoglobinaHombre, hemoglobinaMujer, neutrofilos} from './modules/moduloValores.mjs';
 
 // VARIABLES
 const VRs = [
     glicemia, colesterol, hemoglobinaHombre, hemoglobinaMujer, neutrofilos
 ]
 
-const vrContainer = document.querySelector(".vrContainer")
-const inputContainer = document.querySelector(".inputContainer")
-const lab = document.querySelector("#lab")
+const vrContainer = document.querySelector(".vrContainer");
+const inputContainer = document.querySelector(".inputContainer");
+const lab = document.querySelector("#lab");
+const boton2 = document.querySelector("#boton2");
+const boton = document.querySelector("#boton");
 
 // EVENTOS
-boton = document.querySelector("#boton")
 boton.addEventListener('click', () => {
     mostrarVRs()
-})
+});
+
+boton2.addEventListener('click', () =>{
+    mostrarTodo()
+});
 
 //FUNCIONES
 function mostrarVRs() {
@@ -87,6 +36,12 @@ function mostrarVRs() {
             // LIMPIAR CONTENEDOR
             inputContainer.replaceChildren()
             calcularValor(element.name);
+            // Libreria Tostify
+            Toastify({
+                text: ` va de ${element.valorInferior} a ${element.valorSuperior} ${element.unidad}`,
+                duration: 4000
+            }
+            ).showToast();
         };
         divVR.appendChild(nombreVr)
         divVR.appendChild(btnVR)
@@ -119,9 +74,14 @@ function calcularValor(name) {
     button.type = "button";
     button.innerText = "Enter";
     button.onclick = () => {
-        valor = input.value;
+        const valor = input.value;
         if (valor === "") {
             lab.innerText = "Por favor ingresa un valor";
+            //Libreria sweetalert2
+            Swal.fire({
+                icon: 'error',
+                title: '😕 Porfa ingresa algun valor',
+            })
         } else if (valor < vrSelec.valorInferior) {
             lab.innerText = vrSelec.bajo;
         } else if (valor > vrSelec.valorInferior && valor < vrSelec.valorSuperior) {
@@ -129,7 +89,12 @@ function calcularValor(name) {
         } else if (valor > vrSelec.valorSuperior) {
             lab.innerText = vrSelec.alto;
         } else {
-            lab.innerText = "Por favor ingresa un valor numerico"
+            lab.innerText = "Por favor ingresa un valor numerico";
+            //Libreria sweetalert2
+            Swal.fire({
+                icon: 'error',
+                title: '😕 Porfa agrega un valor numerico!',
+            })
         }
         // STORAGE
         localStorage.setItem(vrSelec.name, valor);
@@ -142,12 +107,16 @@ function calcularValor(name) {
 }
 
 
-function mostrarTodos() {
+function mostrarTodo() {
+    // Toastify
+    Toastify({
+        text: " Aqui estan todos los VRs 👨🏻‍💻",
+        },).showToast();
     show.replaceChildren();
     VRs.forEach((v) => {
 
 // DESTRUCTURANDO
         const {name, valorInferior, valorSuperior, bajo, normal, alto} = v
         show.innerHTML += ` <br><br> VALOR: ${name}, Valor inferior: ${valorInferior} Valor Superior: ${valorSuperior}<br> Mensaje valor bajo: ${bajo} <br> Mensaje Valor Normal: ${normal}<br> Mensaje Valor Alto ${alto}`
+    })};
 
-    })}
---
+++ modules/moduloValores.mjs
@@ -0,0 +1,64 @@
+export {glicemia, colesterol, hemoglobinaHombre, hemoglobinaMujer, neutrofilos};
+
+// PLANTILLA DE VALORES DE REFERENCIA 
+class ValorReferencia {
+    constructor(name, valorInferior, valorSuperior, bajo, normal, alto, unidad) {
+        this.name = name;
+        this.valorInferior = valorInferior;
+        this.valorSuperior = valorSuperior;
+        this.bajo = bajo;
+        this.normal = normal;
+        this.alto = alto;
+        this.unidad = unidad;
+    }
+}
+// let VALOR A AGREGAR = new ValorReferencia(RANGO INFERIOR, RANGO SUPERIOR, MENSAJE VALOR BAJO, MENSAJE VALOR NORMAL, MENSAJE VALOR ALTO
+let glicemia = new ValorReferencia(
+    "GLICEMIA O GLUCEMIA",
+    80,
+    110,
+    "Estas en hipoglicemia, el Valor de Referencia de la glicemia va de  80 a 110 mg/dL",
+    "Estas dentro de los valores de Referencia para Glucosa en Sangre, los cuales son de 80 a 110mg/dL",
+    "Estas en Hiperglicemia, tus valores estan por encima de los de referencia para glucosa en sangre, los cuales son de 80 a 110mg/dL",
+    "mg/dL"
+);
+
+let colesterol = new ValorReferencia(
+    "COLESTEROL TOTAL",
+    125,
+    200,
+    "Estas en hipocolesterolemia, el Valor de Referencia del colesterol serico va de 125 a 200mg/dL",
+    "Estas dentro de los valores de Referencia para el colesterol total serico, los cuales son de 125 a 200mg/dL",
+    "Estas en hipercolesterolemia, tus valores estan por encima de los de referencia para Colesterol en sangre, los cuales son de 125 a 200mg/dL",
+    "mg/dL"
+);
+
+let hemoglobinaHombre = new ValorReferencia(
+    "HEMOGLOBINA EN HOMBRES",
+    13.8,
+    17.2,
+    "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en hombres va de 13.8 a 17.2mg/dL",
+    "Estas dentro de los valores de referencia de Hemoglobina para hombre los cuales son 13.8 a 17.2mg/dL",
+    "Tu valor de hemoglobina esta por encima de los de referencia para hombres, los cuales van de 13.8 a 17.2mg/dL",
+    "mg/dL"
+);
+
+let hemoglobinaMujer = new ValorReferencia(
+    "HEMOGLOBINA EN MUJERES",
+    12.1,
+    15.1,
+    "Tu hemoglobina esta baja, el Valor de Referencia de Hemoglobina en mujeres va de 12.1 a 15.1mg/dL",
+    "Estas dentro de los valores de referencia de Hemoglobina para mujeres los cuales son 12.1 a 15.1mg/dL",
+    "Tu valor de hemoglobina esta por encima de los de referencia para mujeres, los cuales van de 12.1 a 15.1mg/dL",
+    "mg/dL"
+);
+
+let neutrofilos = new ValorReferencia(
+    "SEGMENTADOS NEUTROFILOS",
+    2000,
+    7500,
+    "Estas cursando con Neutropenia, Segmentados Neutrofilos bajos, el Valor de referencia de neutrofilos va de 2000 a 7500 celulas por mL de sangre",
+    "Tus valores de Segmentados neutrofilos se encuentran dentro de los valores normales  de 2000 a 7500 celulas por mL de sangre",
+    "Cursas con una Neutrofilia, considerable aumento de los Segmentados Neutrofilos en sangre, es normalmente causada por infecciones recientes, o inclusive enfermedades inflamatorias ",
+    "celulas/mL sangre"
+);
